f2602e05ff619e33f7eb63efcdfa3a325c9c736c
グェン ヴァン ティエン <nguyen.van.tien@expert.e-e.network>
2025-05-02 11:19:55 +0900

    add check and hide8

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,9 +2,11 @@
 
 from django.urls import path
 from . import views
+from .views import create_admin
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('tests/<str:level>/<str:category>/', views.test_list, name='test_list'),
     path('test/<int:test_id>/', views.take_test, name='take_test'),
+    path('create-admin/', create_admin, name='create_admin'),
 ]
